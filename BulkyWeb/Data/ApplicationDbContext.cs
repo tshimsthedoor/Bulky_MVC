224510a9a3b0d9ec50f6acabfc480ce936466f24
tresor <tshims79@gmail.com>
2023-06-30 01:08:35 +0200

    I created the db bulky and I push the migration

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace BulkyWeb.Data
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+              
+        }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
