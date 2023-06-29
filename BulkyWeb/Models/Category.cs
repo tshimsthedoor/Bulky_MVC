405625d0b9721d4a1705aa1d8e307ca570fef8d8
tresor <tshims79@gmail.com>
2023-06-30 00:46:55 +0200

    I added the data folder with dbcontext class

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,6 +1,13 @@
-﻿namespace BulkyWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
 {
-    public class Class
+    public class Category
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
     }
 }
